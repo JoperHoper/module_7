d2aa61ebe44705a0ddf997167c8eadfc10582180
JoperHoper <josephinelim.sm@gmail.com>
2024-07-04 21:00:44 +0800

    First commit for Module 7

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./App.css";
+
+const App = () => {
+
+  //RETURN
+  return (
+    <div className="main-app-container">Test</div>
+  );
+};
+export default App;
